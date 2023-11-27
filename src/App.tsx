d447a7cf0e5b10ae6d795ba35a3d789ea56b88ed
khushnudeshtemirov <khushnudeshtemirov@gmail.com>
2023-11-27 12:46:16 +0500

    Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import { BsChatDotsFill } from "react-icons/bs";
 import { IoSend } from "react-icons/io5";
 import { FaMicrophone } from "react-icons/fa";
@@ -7,20 +7,17 @@ import { IoClose } from "react-icons/io5";
 import "./App.scss";
 
 const data = [
-  { id: 1, sent: true, text: "Hi, how are you?" },
-  { id: 2, sent: false, text: "I'm fine thanks. What about you?" },
+  { sent: true, text: "Hi, how are you?" },
+  { sent: false, text: "I'm fine thanks. What about you?" },
   {
-    id: 3,
     sent: false,
     text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti vero sapiente recusandae perspiciatis dolorem ad id debitis nostrum eos quo?",
   },
   {
-    id: 4,
     sent: true,
     text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti vero sapiente recusandae perspiciatis dolorem ad id debitis nostrum eos quo?",
   },
   {
-    id: 5,
     sent: true,
     text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti vero sapiente recusandae perspiciatis dolorem ad id debitis.",
   },
@@ -28,10 +25,14 @@ const data = [
 
 function App() {
   const [messages, setMessages] = useState(data);
+  const [text, setText] = useState("");
   const [isSend, setIsSend] = useState(false);
   const [isShow, setIsShow] = useState(false);
+  const bodyRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleChange = (mes: string) => {
+    setText(mes);
     if (mes.length > 0) {
       setIsSend(true);
     } else {
@@ -43,6 +44,23 @@ function App() {
     setIsShow(true);
   };
 
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (text.length > 0) {
+      setMessages([...messages, { sent: true, text: text }]);
+      setIsSend(false);
+      setText("");
+    }
+  };
+
+  useEffect(() => {
+    if (bodyRef.current) {
+      bodyRef.current.scrollTop =
+        bodyRef.current.scrollHeight - bodyRef.current.clientHeight;
+    }
+  }, [messages]);
+
   return (
     <div className="chat">
       <div className="chat__icon" onClick={handleClick}>
@@ -53,27 +71,45 @@ function App() {
           <p>Chat</p>
           <IoClose onClick={() => setIsShow(false)} />
         </div>
-        <div className="chat__body">
+        <div className="chat__body" ref={bodyRef}>
           <div className="chat__messages">
-            {messages.map((message) => (
-              <div
-                key={message.id}
-                className={`${
-                  message.sent ? "chat__send-message" : "chat__receive-message"
-                }`}
-              >
-                <p>{message.text}</p>
-              </div>
-            ))}
+            {messages.length > 0 ? (
+              messages.map((message, index) => (
+                <div
+                  key={index}
+                  className={`${
+                    message.sent
+                      ? "chat__send-message"
+                      : "chat__receive-message"
+                  }`}
+                >
+                  <p>{message.text}</p>
+                </div>
+              ))
+            ) : (
+              <p className="chat__no-message">No messages yet</p>
+            )}
           </div>
         </div>
         <div className="chat__footer">
-          <input
-            type="text"
-            placeholder="Type message..."
-            onChange={(e) => handleChange(e.target.value)}
-          />
-          {isSend ? <IoSend /> : <FaMicrophone />}
+          <form onSubmit={handleSubmit}>
+            <input
+              ref={inputRef}
+              value={text}
+              type="text"
+              placeholder="Type message..."
+              onChange={(e) => handleChange(e.target.value)}
+            />
+            {isSend ? (
+              <button type="submit" onClick={handleSubmit}>
+                <IoSend />
+              </button>
+            ) : (
+              <button type="submit">
+                <FaMicrophone />
+              </button>
+            )}
+          </form>
         </div>
       </div>
     </div>
